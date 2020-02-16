fd70f26fa117b18b4b8086371deb9276b7f2d06e
Dane Iracleous <daneiracleous@gmail.com>
2020-02-16 14:56:49 -0800

    ignoring correct files now and finished deployment script

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,17 @@
-::start backend
-start cmd.exe /k "cd backend && venv\Scripts\activate && set FLASK_APP=application.py && SET FLASK_ENV=development && SET FLASK_DEBUG=1 && flask run"
-::start frontend
-start cmd.exe /k "cd frontend && npm start"
-::start database
-::start cmd.exe /k "C: && cd Program Files\MongoDB\Server\4.2\bin && mongod.exe"
+@ECHO OFF
+ECHO deploying...
+ECHO(
+IF "%1" == "backend" (
+  ::deploy backend
+  cd backend && eb deploy && echo deployed! && cd ..
+) ELSE IF "%1" == "frontend" (
+  ::deploy frontend
+  cd frontend && npm run build && aws s3 sync ./build s3://adventurizer.net/ && echo deployed! && cd ..
+) ELSE (
+  ::deploy backend
+  cd backend && eb deploy && echo deployed! && cd ..
+  ::deploy frontend
+  cd frontend && npm run build && aws s3 sync ./build s3://adventurizer.net/ && echo deployed! && cd ..
+)
+ECHO(
+ECHO deployed!
