53cfcc6d71fca66688297be288481f19f3126ac2
Dane Iracleous <daneiracleous@gmail.com>
2020-02-16 15:27:23 -0800

    fixed deployment script echo

--- deploy.bat
+++ deploy.bat
@@ -3,15 +3,13 @@ ECHO deploying...
 ECHO(
 IF "%1" == "backend" (
   ::deploy backend
-  cd backend && eb deploy && echo deployed! && cd ..
+  cd backend && eb deploy && cd ..
 ) ELSE IF "%1" == "frontend" (
   ::deploy frontend
-  cd frontend && npm run build && aws s3 sync ./build s3://adventurizer.net/ && echo deployed! && cd ..
+  cd frontend && npm run build && aws s3 sync ./build s3://adventurizer.net/ && cd ..
 ) ELSE (
   ::deploy backend
-  cd backend && eb deploy && echo deployed! && cd ..
+  cd backend && eb deploy && cd ..
   ::deploy frontend
-  cd frontend && npm run build && aws s3 sync ./build s3://adventurizer.net/ && echo deployed! && cd ..
+  cd frontend && npm run build && aws s3 sync ./build s3://adventurizer.net/ && cd ..
 )
-ECHO(
-ECHO deployed!
